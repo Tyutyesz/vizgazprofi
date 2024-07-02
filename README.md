f7c332c8b1ff4b54ff6e4d6e1dbfe737c620eeda
Tyutyesz <cs.matyi@hotmail.com>
2024-07-02 22:35:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quick start
+
+## Dependencies
+node.js with npm package management
+
+## Getting started
+In the root directory run npm i from your favorite CLI.
+For development run `npm install`
+For building the project run `npm build`
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/templates/index.pug'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        hot: true,
+        open: true
+    }
+};
